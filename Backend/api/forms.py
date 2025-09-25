9bcc2d2f52748d9bfe117b60bed5121b212cca0a
FletcherFletch <grosserschwuler@gmail.com>
2025-09-25 05:15:17 -0400

    logic for testing

--- Backend/api/forms.py
+++ Backend/api/forms.py
@@ -5,3 +5,9 @@ class ImageForm(forms.ModelForm):
     class Meta:
         model = uupload
         fields = ['name', 'image', 'description']
+
+    def clean_name(self):
+        name = self.cleaned_data['name']
+        if any(char.isdigit() for char in name):
+            raise forms.ValidationError("Name cannot have numbers.")
+        return name 
--- Backend/api/models.py
+++ Backend/api/models.py
@@ -10,3 +10,20 @@ class uupload(models.Model):
 
     def __str__(self):
         return self.name
+
+class Author(models.Model):
+    name = models.CharField(max_length=20)
+    email = models.EmailField()
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    #pic = models.ImageField(upload_to='PostPics/')
+    published = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE) 
+
+class Comment(models.Model):
+    content = models.TextField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    
